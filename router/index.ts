304d6ce1587069ad774bf1860913f19b1b0f3c5d
intartaa1 <intartaa1@newpaltz.edu>
2022-11-30 17:38:13 -0500

    11.30

---
+++ router/index.ts
@@ -0,0 +1,37 @@
+import LoginViewVue from '@/views/LoginView.vue'
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import ProductsView from '../views/ProductsView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: () => import('../views/AboutView.vue')
+    },
+    {
+        path: '/products',
+        name: 'products',
+        component: ProductsView,
+    },
+    {
+        path: '/product/:id',
+        name: 'product',
+        component: () => import('../views/ProductDetailsView.vue'),
+    },
+    {
+        path: '/login',
+        name: 'login',
+        component: LoginViewVue,
+    },
+  ]
+})
+
+export default router
